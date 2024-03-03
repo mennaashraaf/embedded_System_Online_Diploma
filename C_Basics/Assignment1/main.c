63090431a2b1d84b6c537a15309ca0dfa6eaee7d
Menna Ashraf <you@example.com>
2024-03-03 07:12:52 +0200

    c_basics

---
+++ C_Basics/Assignment1/main.c
@@ -0,0 +1,22 @@
+/*
+ * main.c
+ *
+ *  Created on: Mar 3, 2024
+ *      Author: Menna
+ */
+
+#include "stdio.h"
+#include <conio.h>
+void main() {
+int i,j;
+	for(i=0; i<=9; i++)
+	{
+		for(j=i; j<=9; j++)
+		{
+			printf("%d ",j);
+		}
+		printf("\n");
+	}
+
+
+}
