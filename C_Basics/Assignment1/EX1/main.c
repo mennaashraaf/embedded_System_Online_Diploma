eadda2d29504646eb75abb4827d2955e3b55c9a5
Menna Ashraf <you@example.com>
2024-03-08 15:53:51 +0200

    C_basics

---
+++ C_Basics/Assignment1/EX1/main.c
@@ -0,0 +1,16 @@
+/*
+ * main.c
+ *
+ *  Created on: Mar 8, 2024
+ *      Author: Menna
+ */
+#include "stdio.h"
+#include <conio.h>
+void main() {
+int x;
+printf("enter integer: ");
+fflush(stdout);
+scanf("%d",&x);
+printf("you entered: %d",x);
+}
+
